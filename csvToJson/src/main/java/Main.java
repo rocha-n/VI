c9a7e24ee8a3a537851efd956d6ad27431f91062
dma <dorian.magni@globaz.ch>
2018-11-18 12:05:42 +0100

    Modification de la génération des data pour uniformiser les pays Amélioration du code js pour qu'il soit plus générique Changement du graphique pour afficher les pays

--- csvToJson/src/main/java/Main.java
+++ csvToJson/src/main/java/Main.java
@@ -1,10 +1,13 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,6 +27,8 @@ public class Main {
                          .filter(values -> values.length > findIndexMax()
                                  && values[Champs.NUTRITION.getIndex()].length() == 1
                                  && hasAllChamps(values))
+                         .peek(values -> values[Champs.MARQUE.getIndex()] = values[Champs.MARQUE.getIndex()].split(",")[0])
+                         .peek(values -> values[Champs.PAYS.getIndex()] = replaceTextInBrand(values[Champs.PAYS.getIndex()].trim().split(",")[0]))
                          .map(values -> Arrays.stream(Champs.values())
                                               .collect(Collectors.toMap(v -> v.name().toLowerCase(), v -> values[v.getIndex()])))
                          .collect(Collectors.toList());
@@ -32,8 +37,93 @@ public class Main {
         }
     }
 
+
+    private static String replaceTextInBrand(String brand) {
+        Map<String, String> replaceString = new HashMap<>();
+        replaceString.put("en:", "");
+        replaceString.put("nl:", "");
+        replaceString.put("fr:", "");
+        replaceString.put("UK", "United Kingdom");
+        replaceString.put("US", "United States");
+        replaceString.put("Estados Unidos", "United States");
+        replaceString.put("Australia", "Australie");
+        replaceString.put("Austria", "Autriche");
+        replaceString.put("Österreich", "Autriche");
+        replaceString.put("BE", "Belgique");
+        replaceString.put("Belgium", "Belgique");
+        replaceString.put("België", "Belgique");
+        replaceString.put("BG", "Belgique");
+        replaceString.put("Belgien", "Belgique");
+        replaceString.put("Brasil", "Brazil");
+        replaceString.put("CA", "Canada");
+        replaceString.put("CH", "Suisse");
+        replaceString.put("Switzerland", "Suisse");
+        replaceString.put("Schweiz", "Suisse");
+        replaceString.put("Denmark", "Danemark");
+        replaceString.put("Deutschland", "Allemagne");
+        replaceString.put("Germany", "Allemagne");
+        replaceString.put("germany", "Allemagne");
+        replaceString.put("DE", "Allemagne");
+        replaceString.put("ES", "Espagne");
+        replaceString.put("España", "Espagne");
+        replaceString.put("spain", "Espagne");
+        replaceString.put("Spain", "Espagne");
+        replaceString.put("FR", "France");
+        replaceString.put("france", "France");
+        replaceString.put("Francia", "France");
+        replaceString.put("Frankreich", "France");
+        replaceString.put("Frankrijk", "France");
+        replaceString.put("Frankrike", "France");
+        replaceString.put("IT", "Italie");
+        replaceString.put("Italia", "Italie");
+        replaceString.put("Italy", "Italie");
+        replaceString.put("MEXICO", "Mexique");
+        replaceString.put("MÉXICO", "Mexique");
+        replaceString.put("México", "Mexique");
+        replaceString.put("Mexico", "Mexique");
+        replaceString.put("Magyarország", "Hongrie");
+        replaceString.put("Hungary", "Hongrie");
+        replaceString.put("Netherlands", "Nederland");
+        replaceString.put("Norway", "Norvège");
+        replaceString.put("Poland", "Pologne");
+        replaceString.put("România", "Roumanie");
+        replaceString.put("Romania", "Roumanie");
+        replaceString.put("Sverige", "Suède");
+        replaceString.put("Sweden", "Suède");
+        replaceString.put("Česko", "République tchèque");
+        replaceString.put("Россия", "Russie");
+        replaceString.put("Србија", "Serbie");
+        replaceString.put("Serbia", "Serbie");
+        replaceString.put("საქართველო", "Géorgie");
+        replaceString.put("GB", "Grande-Bretagne");
+        replaceString.put("United Kingdom", "Royaume-Uni");
+        replaceString.put("New Zealand", "Nouvelle-Zélande");
+        replaceString.put("Nederland", "Pays-Bas");
+
+        for (Map.Entry<String, String> v : replaceString.entrySet()) {
+            brand = brand.replace(v.getKey(), v.getValue());
+        }
+        return brand;
+    }
+
     private static void toJsonFile(List<Map<String, String>> list) throws IOException {
-        new ObjectMapper().writeValue(new File("data.json"), list);
+        Map<String, Integer> l = new HashMap<>();
+        list.stream().forEach(o -> {
+            String m = o.get(Champs.PAYS.toString().toLowerCase());
+            //  if (m.contains(",")) {
+            if (!l.containsKey(m)) {
+                l.put(m, 1);
+            } else {
+                l.put(m, l.get(m) + 1);
+            }
+            //  }
+        });
+        l.entrySet().stream().filter(o -> o.getValue() > 20).map(o -> o.getKey() + " " + o.getValue()).sorted(String::compareTo)
+         .forEach(o -> System.out.println(o));
+        File file = new File("data.json");
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(file.getPath()))) {
+            writer.write("var data = " + new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).writeValueAsString(list));
+        }
     }
 
     private static int findIndexMax() {
--- js/main.js
+++ js/main.js
@@ -1,57 +1,88 @@
-Array.prototype.groupBy = function (prop) {
-  return this.reduce(function (groups, item) {
-    const val = item[prop];
-    groups[val] = groups[val] || [];
-    groups[val].push(item);
-    return groups
-  }, {})
-};
+/**
+ * Permet de regrouper la liste en fonction d'un nom de champs et calcule le nombre de produit pour la nutrition
+ * @param prop Le champ que l'on vas utiliser pour le groupement.
+ * @param list La liste des produits
+ * @returns Une nouvelle liste qui contient le nom du champs que l'on à donné et des infos sur les nutritions.
+ */
+function groupByAndCountNutrition(prop, list) {
+    let newList = list.reduce((group, item) => {
+        const val = (item[prop] + "").toUpperCase();
+        if (!group.get(val)) {
+            let objet = {total: 0};
+            objet[prop] = val;
+            group.set(val, objet);
+        }
+        let nutritionValues = ["a", "b", "c", "d", "e"];
+        nutritionValues.forEach(value => {
+            if (item.nutrition === value) {
+                let nutrition = group.get(val)[value];
+                if (!nutrition) {
+                    nutrition = 0;
+                }
+                group.get(val).total++;
+                group.get(val)[value] = nutrition + 1;
+            }
+        });
+        return group;
+    }, new Map());
+    return Array.from(newList.values()).sort((a, b) => a[prop].localeCompare(b[prop]));
+}
 
-//let t = data.filter(item => item["nutrition_grade_fr"] != undefined && (item.nutrition_grade_fr.length == 1 || item.nutrition_grade_uk.length == 1));
-let g = data.reduce((group, item) => {
-  const val = (item["marque"] + "").toUpperCase();
-  if (!group.get(val)) {
-    group.set(val, {marque: val, total: 0})
-  }
-  let nutritionValues = ["a", "b", "c", "d", "e"];
-  nutritionValues.forEach(value => {
-    if (item.nutrition === value) {
-      let nutrition = group.get(val)[value];
-      if (!nutrition) {
-        nutrition = 0;
-      }
-      group.get(val).total++;
-      group.get(val)[value] = nutrition + 1;
-    }
-  });
-  return group;
-}, new Map());
-let list = [];
-g.forEach((value) => {
-  if (value.total > 50) {
-    list.push(value);
-  }
-});
-list = list.sort((a, b) => a.marque.localeCompare(b.marque));
+/**
+ * Permet de filtrer dans la liste les objets qui on peut de produits.
+ * @param list
+ * @param nombreDeProduit
+ * @returns La liste
+ */
+function filtreParNombreDeProduit(list, nombreDeProduit) {
+    let newList = [];
+    list.forEach((value) => {
+        if (value.total > nombreDeProduit) {
+            newList.push(value);
+        }
+    });
+    return newList;
+}
 
-let list22 = [];
-list.forEach((obj) => {
-  let nutritionValues = ["a", "b", "c", "d", "e"];
-  nutritionValues.forEach(value => {
-    if (obj[value]) {
-      list22.push({marque: obj.marque, total: obj[value], nutrition:value})
-    }
-  });
-});
-console.log(list22);
+/**
+ * Permet de filtrer la liste des nutrition et de calculer le pourcentage.
+ *
+ *  @param prop Pour définir le champs que l'on veut utiliser.
+ * @param nutrition La nutrition que l'on veut filtrer
+ * @param list La liste que l'on veut filtrer
+ */
+function filtrePourLesNutritiomAndComputPourcentage(prop, nutrition, list) {
+    let newList = [];
+    list.forEach((obj) => {
+        let nutritionValues = ["a", "b", "c", "d", "e"];
+        nutritionValues.forEach(value => {
+            if (value === nutrition) {
+                let newObjet = {total: ((obj.a / obj.total) * 100)};
+                newObjet[prop] = obj[prop];
+                newList.push(newObjet);
+            }
+        });
+    });
+    return newList;
+}
 
-let list2 = [];
-list.forEach((obj) => {
-  let nutritionValues = ["a", "b", "c", "d", "e"];
-  nutritionValues.forEach(value => {
-    if (value === "a") {
-      list2.push({marque: obj.marque, total: ((obj.a/obj.total)*100)})
-    }
-  });
-});
-console.log(list2);
+/**
+ * Permet de créer la liste qui sera utilisé par 3Dplus. Elle permet de pouvoir jouer avec les regroupements.
+ *
+ * @param prop Le nom du champ que l'on vas utiliser pour le groupement de la liste
+ * @returns {Array} La liste pour 3dplus
+ */
+function createLigneToHaveTwoDimention(prop) {
+    let newList = [];
+    list.forEach((obj) => {
+        let nutritionValues = ["a", "b", "c", "d", "e"];
+        nutritionValues.forEach(value => {
+            if (obj[value]) {
+                let newObjet = {total: obj[value], nutrition: value};
+                newObjet[prop] = obj[prop];
+                newList.push(newObjet);
+            }
+        });
+    });
+    return newList;
+}
