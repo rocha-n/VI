12f05faf07e0434038caaea3fd7ee08817c8b796
lukas.bitter <lukas.bitter@he-arc.ch>
2018-11-23 16:56:13 +0100

    enhance the menu

--- js/menu.js
+++ js/menu.js
@@ -9,3 +9,9 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function firstLevel() {
+  document.getElementById("firstLevel").classList.toggle("show");
+}
